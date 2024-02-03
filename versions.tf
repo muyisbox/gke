08d0b02a6fdcd47b9f3a939c695ea4890431dcb8
Olu Alade <oalade@dynamou.io>
2024-02-03 10:45:30 -0500

    update provider version

--- versions.tf
+++ versions.tf
@@ -22,16 +22,19 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
+      version = "~> 5.14.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.78"
+      version = "~> 5.14.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
+      version = "~> 2.25.0"
     }
     helm = {
       source = "hashicorp/helm"
+      version = "~> 2.12.1"
     }
   }
   required_version = ">= 0.13"
