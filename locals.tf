1c13283c4fe61a2cd5a4baa9209aa87856cf034d
Olu Alade <oalade@dynamou.io>
2024-04-04 09:49:07 -0400

    updates

--- locals.tf
+++ locals.tf
@@ -1,48 +1,47 @@
 locals {
   cluster_type = "gitops"
-
   apps = {
     prometheus = {
       name           = "prometheus-monitoring"
       chart          = "kube-prometheus-stack"
       repoURL        = "https://prometheus-community.github.io/helm-charts"
-      targetRevision = "55.1.0"
+      targetRevision = "57.2.0"
       project        = "boeing"
       namespace      = "monitoring"
       values         = indent(10, yamlencode(file("${path.module}/templates/values-prometheus.yaml")))
     }
     istio-base = {
-      name           = "istio-base-1-18-2"
+      name           = "istio-base-1-21"
       chart          = "base"
       repoURL        = "https://istio-release.storage.googleapis.com/charts"
-      targetRevision = "1.18.2"
+      targetRevision = "1.21.0"
       project        = "boeing"
       namespace      = "istio-system"
       values         = indent(10, yamlencode(file("${path.module}/templates/values-base.yaml")))
     }
     istiod = {
-      name           = "istiod-1-18-2"
+      name           = "istiod-1-21"
       chart          = "istiod"
       repoURL        = "https://istio-release.storage.googleapis.com/charts"
-      targetRevision = "1.18.2"
+      targetRevision = "1.21.0"
       project        = "boeing"
       namespace      = "istio-system"
       values         = indent(10, yamlencode(file("${path.module}/templates/values-istiod.yaml")))
     }
-    istiod-canary = {
-      name           = "istiod"
-      chart          = "istiod"
-      repoURL        = "https://istio-release.storage.googleapis.com/charts"
-      targetRevision = "1.19.4"
-      project        = "boeing"
-      namespace      = "istio-system"
-      values         = indent(10, yamlencode(file("${path.module}/templates/values-istiod-new.yaml")))
-    }
+    # istiod-canary = {
+    #   name           = "istiod"
+    #   chart          = "istiod"
+    #   repoURL        = "https://istio-release.storage.googleapis.com/charts"
+    #   targetRevision = "1.19.4"
+    #   project        = "boeing"
+    #   namespace      = "istio-system"
+    #   values         = indent(10, yamlencode(file("${path.module}/templates/values-istiod-new.yaml")))
+    # }
     istio-gateway = {
       name           = "istio-ingressgateway"
       chart          = "gateway"
       repoURL        = "https://istio-release.storage.googleapis.com/charts"
-      targetRevision = "1.19.4"
+      targetRevision = "1.21.0"
       project        = "boeing"
       namespace      = "istio-gateways"
       values         = indent(10, yamlencode(file("${path.module}/templates/values-gateway.yaml")))
@@ -60,7 +59,7 @@ locals {
       name           = "cert-manager"
       chart          = "cert-manager"
       repoURL        = "https://charts.jetstack.io"
-      targetRevision = "1.12.3"
+      targetRevision = "1.14.4"
       project        = "boeing"
       namespace      = "cert-manager"
       values         = indent(10, yamlencode(file("${path.module}/templates/values-certmanager.yaml")))
@@ -78,7 +77,7 @@ locals {
       name           = "loki-stack"
       chart          = "loki-stack"
       repoURL        = "https://grafana.github.io/helm-charts"
-      targetRevision = "2.9.11"
+      targetRevision = "2.10.2"
       project        = "boeing"
       namespace      = "logging"
       values         = indent(10, yamlencode(file("${path.module}/templates/values-loki.yaml")))
@@ -96,7 +95,7 @@ locals {
       name           = "argo-rollouts"
       chart          = "argo-rollouts"
       repoURL        = "https://argoproj.github.io/argo-helm"
-      targetRevision = "2.32.*"
+      targetRevision = "2.35.*"
       project        = "boeing"
       namespace      = "argo-rollouts"
       values         = indent(10, yamlencode(file("${path.module}/templates/values-argo-rollouts.yaml")))
@@ -110,20 +109,7 @@ locals {
       namespace      = "kong-mesh-system"
       values         = indent(10, yamlencode(file("${path.module}/templates/values-kuma.yaml")))
     }
-    argo-rollouts = {
-      name           = "argo-rollouts"
-      chart          = "argo-rollouts"
-      repoURL        = "https://argoproj.github.io/argo-helm"
-      targetRevision = "2.32.*"
-      project        = "boeing"
-      namespace      = "argo-rollouts"
-      values         = indent(10, yamlencode(file("${path.module}/templates/values-argo-rollouts.yaml")))
-    }
   }
-}
-
-
-locals {
   charts = {
     argocd = {
       namespace = var.argocd.namespace
@@ -142,3 +128,4 @@ locals {
   })
 
 }
+
