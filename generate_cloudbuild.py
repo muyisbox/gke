b79eb8b4943c2595bd2daac633122d84e4d75451
Olu Alade <oalade@dynamou.io>
2024-05-27 15:47:34 -0400

    fix

--- generate_cloudbuild.py
+++ generate_cloudbuild.py
@@ -1,7 +1,7 @@
 import yaml
 import os
 
-workspaces = os.environ.get('${_WORKSPACES}', 'dev,staging,gitops').split(',')
+workspaces = os.environ.get('_WORKSPACES', 'dev,staging,gitops').split(',')
 
 steps = [
     {
@@ -10,7 +10,7 @@ steps = [
         'entrypoint': 'bash',
         'args': [
             '-c',
-            'echo "***********************"; echo "Branch Name: $BRANCH_NAME"; echo "***********************"'
+            'echo "************************"; echo "Branch Name: $BRANCH_NAME"; echo "************************"'
         ]
     },
 ]
@@ -19,32 +19,29 @@ for workspace in workspaces:
     steps.extend([
         {
             'id': f'setup and plan {workspace}',
-            'name': 'hashicorp/terraform:${_TF_VERSION}',
+            'name': 'hashicorp/terraform:$_TF_VERSION',
             'entrypoint': 'sh',
             'args': [
                 '-c',
                 f'''
                 echo "Branch Name inside setup and plan step: $BRANCH_NAME"
                 if [ "$BRANCH_NAME" = "main" ] || [ "$BRANCH_NAME" = "master" ]; then
-                  echo "Processing workspace: {workspace}"
-                  terraform init -reconfigure
-
-                  if ! terraform workspace select {workspace}; then
-                    terraform workspace new {workspace}
-                  fi
-
-                  terraform validate
-
-                  terraform plan -var="compute_engine_service_account=terraform@$PROJECT_ID.iam.gserviceaccount.com" -var="project_id=$PROJECT_ID" -out=/workspace/$BUILD_ID/tfplan_{workspace}
+                    echo "Processing workspace: {workspace}"
+                    terraform init -reconfigure
+                    if ! terraform workspace select {workspace}; then
+                        terraform workspace new {workspace}
+                    fi
+                    terraform validate
+                    terraform plan -var="compute_engine_service_account=terraform@$PROJECT_ID.iam.gserviceaccount.com" -var="project_id=$PROJECT_ID" -out=/workspace/$BUILD_ID/tfplan_{workspace}
                 else
-                  echo "Skipping setup and plan on branch $BRANCH_NAME"
+                    echo "Skipping setup and plan on branch $BRANCH_NAME"
                 fi
                 '''
             ]
         },
         {
             'id': f'apply {workspace}',
-            'name': 'hashicorp/terraform:${_TF_VERSION}',
+            'name': 'hashicorp/terraform:$_TF_VERSION',
             'waitFor': [f'setup and plan {workspace}'],
             'entrypoint': 'sh',
             'args': [
@@ -52,36 +49,36 @@ for workspace in workspaces:
                 f'''
                 echo "Branch Name inside apply step: $BRANCH_NAME"
                 if [ "$BRANCH_NAME" = "main" ] || [ "$BRANCH_NAME" = "master" ]; then
-                  echo "Applying Terraform plan for workspace: {workspace}"
-                  if ! terraform workspace select {workspace}; then
-                    terraform workspace new {workspace}
-                  fi
-                  terraform apply -auto-approve /workspace/$BUILD_ID/tfplan_{workspace}
+                    echo "Applying Terraform plan for workspace: {workspace}"
+                    if ! terraform workspace select {workspace}; then
+                        terraform workspace new {workspace}
+                    fi
+                    terraform apply -auto-approve /workspace/$BUILD_ID/tfplan_{workspace}
                 else
-                  echo "Skipping apply on branch $BRANCH_NAME"
+                    echo "Skipping apply on branch $BRANCH_NAME"
                 fi
                 '''
             ]
         },
         {
             'id': f'destroy {workspace}',
-            'name': 'hashicorp/terraform:${_TF_VERSION}',
+            'name': 'hashicorp/terraform:$_TF_VERSION',
             'entrypoint': 'sh',
             'args': [
                 '-c',
                 f'''
                 echo "Branch Name inside destroy step: $BRANCH_NAME"
                 if [ "$BRANCH_NAME" = "destroy-all" ]; then
-                  echo "Preparing to destroy all resources..."
-                  echo "Auto-confirming destruction"
-                  echo "Destroying resources in workspace: {workspace}"
-                  if ! terraform workspace select {workspace}; then
-                    terraform workspace new {workspace}
-                  fi
-                  terraform init -reconfigure
-                  terraform destroy -auto-approve -var="compute_engine_service_account=terraform@$PROJECT_ID.iam.gserviceaccount.com" -var="project_id=$PROJECT_ID"
+                    echo "Preparing to destroy all resources..."
+                    echo "Auto-confirming destruction"
+                    echo "Destroying resources in workspace: {workspace}"
+                    if ! terraform workspace select {workspace}; then
+                        terraform workspace new {workspace}
+                    fi
+                    terraform init -reconfigure
+                    terraform destroy -auto-approve -var="compute_engine_service_account=terraform@$PROJECT_ID.iam.gserviceaccount.com" -var="project_id=$PROJECT_ID"
                 else
-                  echo "Destroy operation not allowed on this branch."
+                    echo "Destroy operation not allowed on this branch."
                 fi
                 '''
             ]
